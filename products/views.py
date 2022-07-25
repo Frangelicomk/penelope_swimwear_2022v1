60bef2ef5b8441c73fa5a51cbb694dcc4b669674
Frangelicomk <mk3987@aol.com>
2022-07-25 14:02:29 +0000

    added footer.html, modals.html, collection.html products views and urls and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to render/return the all products/collection page, including \
+        search and sorting """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/collection.html', context)
