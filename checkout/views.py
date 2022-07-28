ebe80262d1c05864c046368ed9e346b9fab37731
Frangelicomk <mk3987@aol.com>
2022-07-28 14:36:13 +0000

    stripe payments now work, there is an issue with the form layout which I will fix later when I change to Bootstrap4

--- checkout/views.py
+++ checkout/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'There is nothing yet in your bag')
         return redirect(reverse('products'))
-    
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+    )
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_puclic_key': 'pk_test_51LJfiCD5pTpnFYApxznAF0z0Vjeu5hfr2c3Ao3GBrFvI9Sh373mW9lcujNBwh9M9vOlQOxdPZHou4Cu3VayF7kaA00xz96SKnQ',
-        'client_secret': "test client secret",
+        'stripe_puclic_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
-
-
-
