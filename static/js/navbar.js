34ae3e98ac2dead6095f02ee1c94f076a350ca5a
Frangelicomk <mk3987@aol.com>
2022-07-29 16:55:29 +0000

    checkout does not work trying to fix this. Webhooks have an error

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,198 +1,22 @@
-var searchVisible = 0;
-var transparent = true;
-
-var transparentDemo = true;
-var fixedTop = false;
-var navbar_initialized = false;
-
-$(document).ready(function(){
-    BrowserDetect.init();
-    
-    if(BrowserDetect.browser == 'Explorer' && BrowserDetect.version <= 9){
-        $('body').html(better_browser);   
-    }
-
-    window_width = $(window).width();
-    window_height = $(window).height();
-
-    burger_menu = $('nav[role="navigation"]').hasClass('navbar-burger') ? true : false;
-    
-    if (!Modernizr.touch){
-        $('body').addClass('no-touch');
-        no_touch_screen = true;
-    }
-    
-    rubik.initAnimationsCheck();
-    
-    // Init navigation toggle for small screens   
-    if(window_width < 992 || burger_menu){
-        rubik.initRightMenu();   
-    }
-
-
-});
-
-//activate collapse right menu when the windows is resized 
-$(window).resize(function(){
-    if($(window).width() < 992){
-        rubik.initRightMenu();  
-        rubik.checkResponsiveImage(); 
-    }
-    if($(window).width() > 992 && !burger_menu){
-        $('nav[role="navigation"]').removeClass('navbar-burger');
-        rubik.misc.navbar_menu_visible = 1;
-        navbar_initialized = false;
-    }
-});
-
-$(window).on('scroll',function(){
-   rubik.checkScrollForTransparentNavbar();    
-   
-   if(window_width > 992){
-        rubik.checkScrollForParallax();
-   }
-   
-   if(content_opacity == 1){
-       rubik.checkScrollForContentTransitions();
-   }             
-});
-
-$('a[data-scroll="true"]').click(function(e){         
-    var scroll_target = $(this).data('id');
-    var scroll_trigger = $(this).data('scroll');
-    
-    if(scroll_trigger == true && scroll_target !== undefined){
-        e.preventDefault();
-        
-        $('html, body').animate({
-             scrollTop: $(scroll_target).offset().top - 50
-        }, 1000);
-    }
-                
-});
-
-rubik = {
-    misc:{
-        navbar_menu_visible: 0
-    },
-    initAnimationsCheck: function(){
-        $('[class*="add-animation"]').each(function(){
-           offset_diff = 30;
-           if($(this).hasClass('title')){
-               offset_diff = 110;
-           }
-           
-           var waypoints = $(this).waypoint(function(direction) {
-                if(direction == 'down'){
-                        $(this.element).addClass('animate');    
-                   } else {
-                       $(this.element).removeClass('animate');
-                   }
-                }, {
-                  offset: window_height - offset_diff
-           });
-        });
+const themeMap = {
+    dark: "light",
+    light: "solar",
+    solar: "dark"
+  };
   
-    },
-    initRightMenu: function(){  
-         if(!navbar_initialized){
-            $nav = $('nav[role="navigation"]');
-            $nav.addClass('navbar-burger');
-             
-            $navbar = $nav.find('.navbar-collapse').first().clone(true);
-              
-            ul_content = '';
-             
-            $navbar.children('ul').each(function(){
-                content_buff = $(this).html();
-                ul_content = ul_content + content_buff;   
-            });
-             
-            ul_content = '<ul class="nav navbar-nav">' + ul_content + '</ul>';
-            $navbar.html(ul_content);
-             
-            $('body').append($navbar);
-                            
-            background_image = $navbar.data('nav-image');
-            if(background_image != undefined){
-                $navbar.css('background',"url('" + background_image + "')")
-                       .removeAttr('data-nav-image')
-                       .css('background-size',"cover")
-                       .addClass('has-image');                
-            }
-             
-            $toggle = $('.navbar-toggle');
-             
-            $navbar.find('a').removeClass('btn btn-round btn-default');
-            $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
-            $navbar.find('button').addClass('btn-simple btn-block');
-
-            $link = $navbar.find('a');
-            
-            $link.click(function(e){
-                var scroll_target = $(this).data('id');
-                var scroll_trigger = $(this).data('scroll');
-                
-                if(scroll_trigger == true && scroll_target !== undefined){
-                    e.preventDefault();
-
-                    $('html, body').animate({
-                         scrollTop: $(scroll_target).offset().top - 50
-                    }, 1000);
-                }
-                
-             });
-
-            
-            $toggle.click(function (){    
-
-                if(rubik.misc.navbar_menu_visible == 1) {                    
-                    $('html').removeClass('nav-open'); 
-                    rubik.misc.navbar_menu_visible = 0;
-                    $('#bodyClick').remove();
-                     setTimeout(function(){
-                        $toggle.removeClass('toggled');
-                     }, 550);
-                
-                } else {
-                    setTimeout(function(){
-                        $toggle.addClass('toggled');
-                    }, 580);
-                    
-                    div = '<div id="bodyClick"></div>';
-                    $(div).appendTo("body").click(function() {
-                        $('html').removeClass('nav-open');
-                        rubik.misc.navbar_menu_visible = 0;
-                        $('#bodyClick').remove();
-                         setTimeout(function(){
-                            $toggle.removeClass('toggled');
-                         }, 550);
-                    });
-                   
-                    $('html').addClass('nav-open');
-                    rubik.misc.navbar_menu_visible = 1;
-                    
-                }
-            });
-            navbar_initialized = true;
-        }
-   
-    },
-
-    
-    checkScrollForTransparentNavbar: debounce(function() {	
-        	if($(document).scrollTop() > 560 ) {
-                if(transparent) {
-                    transparent = false;
-                    $('nav[role="navigation"]').removeClass('navbar-transparent');
-                }
-            } else {
-                if( !transparent ) {
-                    transparent = true;
-                    $('nav[role="navigation"]').addClass('navbar-transparent');
-                }
-            }
-    }, 17),
-
-
-}
+  const theme = localStorage.getItem('theme')
+    || (tmp = Object.keys(themeMap)[0],
+        localStorage.setItem('theme', tmp),
+        tmp);
+  const bodyClass = document.body.classList;
+  bodyClass.add(theme);
+  
+  function toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = themeMap[current];
+  
+    bodyClass.replace(current, next);
+    localStorage.setItem('theme', next);
+  }
+  
+  document.getElementById('themeButton').onclick = toggleTheme;
