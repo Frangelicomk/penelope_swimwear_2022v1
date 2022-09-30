a34fe73c1693537f717d7450bbaf0d128b17b182
Frangelicomk <mk3987@aol.com>
2022-09-30 18:56:29 +0000

    fixing login.html singup.html verification.html to be more presentint

--- penelope_c_v1/settings.py
+++ penelope_c_v1/settings.py
@@ -207,7 +207,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'penelope_c_swimwear@example.com'
+    DEFAULT_FROM_EMAIL = 'no-reply@penelope_c_swimwear.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
