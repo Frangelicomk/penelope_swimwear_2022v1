c432c5dfd3f6ed25137c4174f807a43a4ef53281
Frangelicomk <mk3987@aol.com>
2022-10-03 11:23:55 +0000

    trying to add a contact form using django

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import contactView, successView
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path("", contactView, name="contact"),
+    path("", successView, name="success"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -7,3 +10,24 @@ def index(request):
     """ A view to render/return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contactView(request):
+    if request.method == "GET":
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data["subject"]
+            from_email = form.cleaned_data["from_email"]
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ["admin@example.com"])
+            except BadHeaderError:
+                return HttpResponse("Invalid header found.")
+            return redirect("success")
+    return render(request, "index.html", {"form": form})
+
+
+def successView(request):
+    return HttpResponse("Success! Thank you for your message.")
