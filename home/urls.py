2a9f8277497f1251ef81b2bf67a782d74565c8c5
Frangelicomk <mk3987@aol.com>
2022-10-03 11:29:12 +0000

    trying to fix contact form using django

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
-from .views import contactView, successView
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path("", contactView, name="contact"),
-    path("", successView, name="success"),
 ]
