8f4f3ecd63e24f1e3240fc51e68c242d3248f5fd
Frangelicomk <mk3987@aol.com>
2022-10-04 21:32:06 +0000

    fixed admin verbose name for emails and updated Meta class

--- home/models.py
+++ home/models.py
@@ -4,12 +4,14 @@ from django.db import models
 
 
 class ContactFormEmails(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'ContactFormEmails'
     name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(max_length=100, null=True, blank=True)
     subject = models.CharField(max_length=100, null=True, blank=True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.email
--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('message_sent/', views.contactForm, name='message_sent'),
+    path('index', views.contactForm, name='message_sent'),
 ]
