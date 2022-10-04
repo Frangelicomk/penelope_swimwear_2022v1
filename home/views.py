66c44084b9bb2083bfedb11d7dbe648ab54d48ca
Frangelicomk <mk3987@aol.com>
2022-10-04 21:16:59 +0000

    trying to fix server error 500 on deployed site

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from .forms import ContactForm
 # Create your views here.
 
@@ -22,7 +23,16 @@ def contactForm(request):
             email_subject = f'New contact {form.cleaned_data["name"]}: {form.cleaned_data["subject"]}'
             email_message = f'Message: {form.cleaned_data["message"]}, Email: {form.cleaned_data["email"]}'
             send_mail(email_subject, email_message, settings.CONTACT_EMAIL, settings.ADMIN_EMAILS,)
-            return render(request, 'home/index.html')
-    form = ContactForm()
-    context = {'form': form}
-    return render(request, 'home/index.html', context)
+            messages.success(request, 'Thank you for your message')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to send message. Please ensure the form is valid.')
+    else:
+        form = ContactForm()
+
+    template = 'index.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
