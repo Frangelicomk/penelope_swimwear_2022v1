41295e87813991c5c91a49da6fb59b6629fa9f65
Frangelicomk <mk3987@aol.com>
2022-10-06 12:16:03 +0000

    added a restriction to /admin to everyone unless they log in as superuser or staff

--- penelope_c_v1/urls.py
+++ penelope_c_v1/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import page_not_found
+
+from penelope_c_v1.admin import admin_view
+admin.site.admin_view = admin_view
 
 urlpatterns = [
+    url(r'^admin/login/', page_not_found),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
